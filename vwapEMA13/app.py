71e54cc0899d4d24e7057dedfe536cb9b6735dfb
luntan <lucas.orman@gmail.com>
2022-05-31 18:11:14 -0500

    Removed some fluff code

--- vwapEMA13/app.py
+++ vwapEMA13/app.py
@@ -1,37 +1,3 @@
-def implement_macd_strategy(prices, data):    
-    buy_price = []
-    sell_price = []
-    macd_signal = []
-    signal = 0
-
-    for i in range(len(data)):
-        if data['macd'][i] > data['signal'][i]:
-            if signal != 1:
-                buy_price.append(prices[i])
-                sell_price.append(np.nan)
-                signal = 1
-                macd_signal.append(signal)
-            else:
-                buy_price.append(np.nan)
-                sell_price.append(np.nan)
-                macd_signal.append(0)
-        elif data['macd'][i] < data['signal'][i]:
-            if signal != -1:
-                buy_price.append(np.nan)
-                sell_price.append(prices[i])
-                signal = -1
-                macd_signal.append(signal)
-            else:
-                buy_price.append(np.nan)
-                sell_price.append(np.nan)
-                macd_signal.append(0)
-        else:
-            buy_price.append(np.nan)
-            sell_price.append(np.nan)
-            macd_signal.append(0)
-            
-    return buy_price, sell_price, macd_signal
-
 def vwap(df):
     q = df.Volume
     p = df.Close
